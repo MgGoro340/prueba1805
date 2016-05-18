5e8f58639b492c250f595b52c21b6883e394828c
mggoro <martingoro@gmail.com>
2016-05-18 16:54:20 -0300

    commit 2

--- src/prueba1805/prueba.java
+++ src/prueba1805/prueba.java
@@ -5,6 +5,8 @@ public class prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola 1");
+		System.out.println("hola 2");
+		System.out.println("hola 2");
 	}
 
 }
