027d9f946f872284536fc1c95dbc4fbab044cd16
martingoro@gmail.com <martingoro@gmail.com>
2016-05-31 13:31:39 -0300

    1331

--- src/prueba1805/prueba.java
+++ src/prueba1805/prueba.java
@@ -4,7 +4,7 @@ public class prueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hola 1");
+		System.out.println("hola 1" );
 		System.out.println("hola 2");
 		System.out.println("hola 3");
 		System.out.println("hola 3 desde el branch 31052016consola03") ;
